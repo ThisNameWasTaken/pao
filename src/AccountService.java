07d605217038f975f48a7b8b1f6068d4e8c1916b
Adrian <adriandinca2008@yahoo.com>
2019-05-07 22:53:32 +0300

    refactor: Use the log service

--- src/AccountService.java
+++ src/AccountService.java
@@ -1,6 +1,7 @@
+import Services.LogService;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,12 +9,20 @@ import java.util.Scanner;
 
 public class AccountService {
   private User user;
+  private LogService logService;
+
+  public AccountService (String timestampFilePath) {
+    this.logService = new LogService(timestampFilePath);
+  }
+
+  public AccountService () {
+    this.logService = new LogService();
+  }
 
   public void logInUser(User user) { // 1
     this.user = user;
 
-    System.out.println("Logged in as " + user.getName());
-    System.out.println("");
+    this.logService.logMessage("Logged in as " + user.getName());
   }
 
   public void joinGroup(Group group) { // 2
@@ -24,25 +33,21 @@ public class AccountService {
     this.user.leaveGroup(group);
     group.removeUser(this.user);
 
-    System.out.println(this.user.getName() + " left " + group.getName());
-    System.out.println(group + ": { users: " + group.getUsers() + ", admins: " + group.getAdmins() + "}");
-    System.out.println("");
+    this.logService.logMessage(this.user.getName() + " left " + group.getName());
   }
 
   public Group createGroup(Group group) { // 4
     this.user.joinGroup(group);
     group.addAdmin(this.user);
 
-    System.out.println(this.user.getName() + " created " + group.getName());
-    System.out.println(group + ": { users: " + group.getUsers() + ", admins: " + group.getAdmins() + "}");
-    System.out.println("");
+    this.logService.logMessage(this.user.getName() + " created " + group.getName());
 
     return group;
   }
 
   public void kickUserFromGroup(User userToKick, Group group) { // 5
     if(!group.isAdmin(this.user)) {
-      System.out.println(this.user.getName() + " is not an Admin of " + group.getName() + "\n");
+      System.out.println(this.user.getName() + " is not an Admin of " + group.getName());
       return;
     }
 
@@ -53,35 +58,28 @@ public class AccountService {
 
     userToKick.leaveGroup(group);
     group.removeUser(userToKick);
-    System.out.println(userToKick.getName() + " kicked from " + group.getName() + " by " + this.user.getName());
-    System.out.println(group + ": { users: " + group.getUsers() + ", admins: " + group.getAdmins() + "}");
-    System.out.println("");
+
+    this.logService.logMessage(userToKick.getName() + " kicked from " + group.getName() + " by " + this.user.getName());
   }
 
   public void addUserToGroup(User user, Group group) {
     user.joinGroup(group);
     group.addUser(this.user);
 
-    System.out.println(user.getName() + " joined " + group.getName());
-    System.out.println(group + ": { users: " + group.getUsers() + ", admins: " + group.getAdmins() + "}");
-    System.out.println("");
+    this.logService.logMessage(user.getName() + " joined " + group.getName());
   }
 
   public void addAdminToGroup(User admin, Group group) { // 9
     group.removeUser(admin);
     group.addAdmin(admin);
 
-    System.out.println(admin.getName() + " was promoted to admin by " + this.user.getName());
-    System.out.println(group + ": { users: " + group.getUsers() + ", admins: " + group.getAdmins() + "}");
-    System.out.println("");
+    this.logService.logMessage(admin.getName() + " was promoted to admin by " + this.user.getName());
   }
 
   public void removeAdminToGroup(User admin, Group group) { // 10
     group.removeAdmin(admin);
 
-    System.out.println(admin.getName() + " was downgraded to user by " + this.user.getName());
-    System.out.println(group + ": { users: " + group.getUsers() + ", admins: " + group.getAdmins() + "}");
-    System.out.println("");
+    this.logService.logMessage(admin.getName() + " was promoted to admin by " + this.user.getName());
   }
 
   public void uploadFileToGroup(MyFile file, Group group) { // 6
@@ -90,8 +88,8 @@ public class AccountService {
       return;
     }
 
-    System.out.println(file.getName() + " was uploaded by " + this.user.getName());
-    System.out.println("");
+    group.uploadFile(file);
+    this.logService.logMessage(file.getName() + " was uploaded by " + this.user.getName());
   }
 
   public void deleteFileFromGroup(MyFile file, Group group) { // 7
@@ -100,8 +98,8 @@ public class AccountService {
       return;
     }
 
-    System.out.println(file.getName() + " was deleted by " + this.user.getName());
-    System.out.println("");
+    group.removeFile(file);
+    this.logService.logMessage(file.getName() + " was deleted by " + this.user.getName());
   }
 
   public void playFileFromGroup(MyFile file, Group group) { // 8
@@ -136,7 +134,7 @@ public class AccountService {
           stringBuilder.append(element);
           stringBuilder.append(',');
         }
-        
+
         if(stringBuilder.length() > 0) {
           if (stringBuilder.charAt(stringBuilder.length() - 1) == ',') {
             stringBuilder.deleteCharAt(stringBuilder.length() - 1);
@@ -192,8 +190,6 @@ public class AccountService {
 
         groups.add(new Group(groupName, admins, users, files));
       }
-
-      System.out.println(groups.toString());
 
       scanner.close();
     } catch (Exception e) {
